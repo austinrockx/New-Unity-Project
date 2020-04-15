cd1e9228739bc3f3a4759b795deae8d9fb34b7f5
austinrockx <52753740+austinrockx@users.noreply.github.com>
2020-04-14 19:11:53 -0600

    Initial Commit

---
+++ Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public GameObject player;
+
+    //changes the camera postion to above the vehicle
+    private Vector3 offset = new Vector3(0, 7, -9);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //Make the cam follow the vehicle
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    // The speed of the vehicale
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float fowardInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        fowardInput = Input.GetAxis("Vertical");
+
+        // Move the Vehicale forward in real time
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * fowardInput);
+        
+        //Rotates the vehicle based on the horizontal input
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+
+}
